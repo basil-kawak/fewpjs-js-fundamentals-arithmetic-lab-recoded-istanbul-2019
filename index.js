85b41bf8d21a11fd29b56bb6d45a49eaef223528
basil-kawak <basilalisa0@gmail.com>
2020-04-26 02:43:25 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+var multiply;
+var num1 = 31;
+var num2 = 2;
+multiply =num1*num2;
+
+var random = (Math.random() * 10) +1;
+
+var mod = 16;
+mod %=6;
+
+var max = Math.max(20);
